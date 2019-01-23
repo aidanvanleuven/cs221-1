4cb24819b5d15309b3e5b819bda9eeddaef704a2
Aidan <aidanvanleuven@gmail.com>
2019-01-23 00:05:21 -0700

    added all test files and completed testing functionality

--- MagicSquare.java
+++ MagicSquare.java
@@ -9,9 +9,8 @@ public class MagicSquare {
         Scanner lineScanner; //scans the file
         Scanner columnScanner; //scans each line
         int size; //size of the array neccessary
-        boolean valid; //bool for validity
+        //boolean valid; //bool for validity
         int magicNumber;
-        int checkValue;
 
         //try to load the file; should be unneccessary because of previous checks but the compiler likes it
         try {
@@ -29,7 +28,7 @@ public class MagicSquare {
         //create 2d array of proper size
         int[][] grid = new int[size][size];
 
-        //loop through rows and integers and add them to an arraylist and a 2d array
+        //loop through rows and columns and add values to a 1d and 2d array
         for (int i=0;lineScanner.hasNextLine();i++){
 
             columnScanner = new Scanner(lineScanner.nextLine());
@@ -46,33 +45,56 @@ public class MagicSquare {
         Arrays.sort(numberList);
 
         //get the magic number
-        magicNumber = (size*(size+1))/2;
+        magicNumber = (size*(size*size+1))/2;
 
-
-        valid = true;
+        //initial state is true, if this is changed, return the false output
+        //valid = true;
 
         //check the arrays numbers to verify the magic square
         for (int i=0;i<numberList.length;i++){
-            System.out.println(i+1 + " " + numberList[i]);
             if (i+1 != numberList[i]){
-                valid = false;
+                //valid = false;
                 return outputValues(grid, false);
             }
         }
 
-        if (valid == true){
-            return outputValues(grid, true);
+        int checkValue1 = 0;
+        int checkValue2 = 0;
+
+        System.out.println(magicNumber);
+
+        //check the sums to verify the magic square
+        //check rows and columns
+        for (int i=0;i<grid.length;i++){
+
+            for (int j=0;j<grid.length;j++){
+                checkValue1 += grid[i][j];
+                checkValue2 += grid[j][i];
+            }
+
+            if (checkValue1 != magicNumber || checkValue2 != magicNumber){
+                return outputValues(grid, false);
+            } else {
+                checkValue1 = 0;
+                checkValue2 = 0;
+            }
         }
 
-        // checkValue = 0;
-        // //check the sums to verify the magic square
-        // for (int i=0;i<grid.length;i++){
-        //     for (int j=0;j<grid.length;j++){
+        int checkValue3 = 0;
 
-        //     }
-        // }
+        //check diagonals
+        for (int i=0;i<grid.length;i++){
+            checkValue3 += grid[i][i];
+        }
 
-        return "There was an error checking the square.";
+        if (checkValue3 != magicNumber){
+            return outputValues(grid, false);
+        }
+
+        //if it passes all tests, output a success message
+        return outputValues(grid, true);
+
+        //return "There was an error checking the square.";
     }
 
     private static String outputValues(int[][] matrix, boolean isValid){
@@ -87,7 +109,7 @@ public class MagicSquare {
             output += "\n";
         }
         
-
+        //add the appropriate message
         if (isValid){
             output += "\nis a magic square.";
         } else {
